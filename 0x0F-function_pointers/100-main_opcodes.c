b7ba85d549f0175b51953cdf0f097ccfe27a1f7b
sclpist <68994787+sclpist@users.noreply.github.com>
2023-07-20 02:40:32 +0200

    advanced task

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,35 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_opcodes(int num_bytes)
-{
-	unsigned char *main_function_ptr = (unsigned char *)print_opcodes;
-
-	for (int i = 0; i < num_bytes; i++)
-	{
-		printf("%02x ", main_function_ptr[i]);
-	}
-
-	printf("\n");
-}
-
+/**
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
+	int bytes, i;
+	char *arr;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return 1;
+		exit(1);
 	}
 
-	int num_bytes = atoi(argv[1]);
+	bytes = atoi(argv[1]);
 
-	if (num_bytes <= 0)
+	if (bytes < 0)
 	{
 		printf("Error\n");
-		return 2;
+		exit(2);
 	}
 
-	print_opcodes(num_bytes);
+	arr = (char *)main;
 
-	return 0;
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
 }
